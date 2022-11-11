39a86a2d7d9a510f2ab629bcf81c9cdfe929cf72
akrnv25 <akrnv25@gmail.com>
2022-11-12 00:47:08 +0400

    create reservation

--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -9,6 +9,15 @@ class ReservationsController {
       .then(successRes => res.status(200).json(successRes))
       .catch(failedRes => res.status(400).json(failedRes));
   }
+
+  create(req, res) {
+    const path = `/rsv/v1/hotels/${config.hotelId}/reservations`;
+    const data = {}; // todo: need read docs
+    oracleApiService
+      .post(path, data)
+      .then(successRes => res.status(200).json(successRes))
+      .catch(failedRes => res.status(400).json(failedRes));
+  }
 }
 
 module.exports = new ReservationsController();
--- src/routes/reservations.routes.js
+++ src/routes/reservations.routes.js
@@ -4,5 +4,6 @@ const reservationsController = require('../controllers/reservations.controller')
 const router = express.Router();
 
 router.get('/', reservationsController.getAll.bind(reservationsController));
+router.post('/', reservationsController.create.bind(reservationsController));
 
 module.exports = router;
