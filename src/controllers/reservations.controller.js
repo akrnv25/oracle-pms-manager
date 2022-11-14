c926b2006d6bcdf2fd0c751a8617f5f45e50c727
akrnv25 <akrnv25@gmail.com>
2022-11-14 23:55:38 +0400

    created reservation controller

--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -10,10 +10,62 @@ class ReservationsController {
       .catch(failedRes => res.status(400).json(failedRes));
   }
 
-  // todo
   create(req, res) {
     const path = `/rsv/v1/hotels/${config.hotelId}/reservations`;
-    const data = {};
+    const { roomTypeCode, profileId, ratePlanCode, arrivalDate, departureDate } = req.body;
+    const data = {
+      reservations: {
+        reservation: {
+          reservationGuests: {
+            profileInfo: {
+              profileIdList: {
+                id: profileId,
+                type: 'Profile'
+              }
+            }
+          },
+          reservationPaymentMethods: {
+            paymentMethod: 'CA'
+          },
+          markAsRecentlyAccessed: true,
+          hotelId: config.hotelId,
+          reservationStatus: 'Reserved',
+          roomStay: {
+            guarantee: {
+              onHold: false,
+              guaranteeCode: '6PM'
+            },
+            roomRates: {
+              numberOfUnits: 1,
+              rates: {
+                rate: {
+                  start: arrivalDate,
+                  end: departureDate,
+                  base: {
+                    amountBeforeTax: 200,
+                    currencyCode: 'USD'
+                  }
+                }
+              },
+              start: arrivalDate,
+              end: departureDate,
+              marketCode: 'LEISURE',
+              sourceCode: 'PHONE',
+              roomTypeCharged: roomTypeCode,
+              ratePlanCode: ratePlanCode,
+              roomType: roomTypeCode,
+              pseudoRoom: false
+            },
+            guestCounts: {
+              children: 0,
+              adults: 1
+            },
+            arrivalDate: arrivalDate,
+            departureDate: departureDate
+          }
+        }
+      }
+    };
     oracleApiService
       .post(path, data)
       .then(successRes => res.status(200).json(successRes))
