aa23c943f715110a74a83ac5e04791e68c069068
akrnv25 <akrnv25@gmail.com>
2022-11-16 03:49:27 +0400

    check in workflow created

--- src/controllers/workflows.controller.js
+++ src/controllers/workflows.controller.js
@@ -1,6 +1,29 @@
+const roomsService = require('../services/rooms.service');
+const reservationsService = require('../services/reservations.service');
+const availabilityService = require('../services/availability.service');
+
 class WorkflowsController {
   checkIn(req, res) {
-    res.status(200).json({ success: true });
+    availabilityService
+      .get()
+      .then(() => {
+        return reservationsService.create();
+      })
+      .then(() => {
+        return roomsService.getAll();
+      })
+      .then(() => {
+        return reservationsService.assignRoom();
+      })
+      .then(() => {
+        return reservationsService.checkIn();
+      })
+      .then(() => {
+        res.status(200).json({ success: true });
+      })
+      .catch(failedRes => {
+        res.status(400).json(failedRes);
+      });
   }
 }
 
